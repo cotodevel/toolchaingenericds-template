c04ca1df10f61f9c3ef1ecf01b2566d4736e0f4f
cotodevel <programmer.coto.1988@gmail.com>
2017-09-28 23:49:49 -0400

    First commit, working.

---
+++ README.md
@@ -0,0 +1,15 @@
+This is the Toolchain Generic DS default example project (for arm7/arm9 code) :
+
+To compile this project you will need to follow the steps at https://github.com/cotodevel/ToolchainGenericDS:
+
+Then simply extract the project somewhere.
+
+Compile with Make as usual.
+
+
+
+
+Coto
+
+
+
---
+++ arm9/source/main.c
@@ -0,0 +1,94 @@
+/*
+
+			Copyright (C) 2017  Coto
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+
+*/
+
+#include "socket.h"
+#include "in.h"
+#include <netdb.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include "main.h"
+#include "InterruptsARMCores_h.h"
+#include "specific_shared.h"
+#include "ff.h"
+#include "mem_handler_shared.h"
+#include "reent.h"
+#include "sys/types.h"
+#include "dsregs.h"
+#include "dsregs_asm.h"
+#include "typedefs.h"
+#include "console.h"
+#include "toolchain_utils.h"
+
+#include "devoptab_devices.h"
+#include "fsfat_layer.h"
+#include "usrsettings.h"
+
+#include "video.h"
+#include "keypad.h"
+#include "gui.h"
+
+int main(int _argc, sint8 **_argv) {
+	
+	IRQInit();
+	
+	bool project_specific_console = false;	//set default console or custom console: default console
+	GUI_init(project_specific_console);
+	GUI_clear();
+	
+	//Init DS Firmware Settings
+	while(getFWSettingsstatus() == false){
+		IRQVBlankWait();
+	}
+	
+	sint32 fwlanguage = (sint32)getLanguage();
+	
+	int ret=FS_init();
+	if (ret == 0)
+	{
+		printf("FS Init ok.");
+	}
+	else if(ret == -1)
+	{
+		printf("FS Init error.");
+	}
+	
+	while (1)
+	{
+		if (keysPressed() & KEY_A){
+			printf("test:%d",rand()&0xff);
+		}
+		
+		if (keysPressed() & KEY_B){
+			GUI_clear();
+		}
+		
+		IRQVBlankWait();
+	}
+
+}
