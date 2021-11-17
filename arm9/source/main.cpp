673cb3181a6b023e8a0e591f7dae5ce6278b61b7
Coto <programmer.coto.1988@gmail.com>
2021-11-16 21:24:30 -0300

    - nasty bugfix to ARM7DLDI which caused some slot-1 cards to NOT initialize when they should! TGDS DLDI API supported again on SLOT-1 cards, SLOT-2 and TWL

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -143,9 +143,8 @@ int main(int argc, char **argv) {
 	{
 		printf("FS Init ok.");
 	}
-	else if(ret == -1)
-	{
-		printf("FS Init error.");
+	else{
+		printf("FS Init error: %d", ret);
 	}
 	
 	asm("mcr	p15, 0, r0, c7, c10, 4");
